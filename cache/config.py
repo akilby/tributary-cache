edf0ae4bf6f48282a74710126157ae86e6629afe
Angela E Kilby <a.kilby@northeastern.edu>
2019-10-20 15:00:36 -0400

    attempting to make imported submodules a property of the cache object

--- cache/config.py
+++ cache/config.py
@@ -90,7 +90,7 @@ def user_prompts(dir_def, packages_def, exclusions_def):
     ask_submodules = user_prompt('Prompt submodules '
                                  '(y/n)? ', inlist=['y', 'n'])
     if_no = user_prompt('Keep current defaults (d) '
-                        'or import entire module (m)?',
+                        'or import entire module (m)? ',
                         inlist=['d', 'm']) if ask_submodules == 'n' else None
     exclusions = user_prompt('List of functions or modules to exclude '
                              'from code tree (rarely used)', exclusions_def)
