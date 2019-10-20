ffb057c152707e41440a89a21dcee980ae2e27ff
Angela E Kilby <a.kilby@northeastern.edu>
2019-10-20 16:26:44 -0400

    adding cacher object-specific config

--- cache/initialize.py
+++ cache/initialize.py
@@ -1,5 +1,5 @@
 import importlib
-from .config import get_config, load_config
+from .config import get_config
 
 directory, registry, exclusion_list = get_config()
 
@@ -7,13 +7,3 @@ directory, registry, exclusion_list = get_config()
  for module in registry]
 
 globals_list = globals()
-
-
-def return_alternative_globals(config_file):
-    directory, registry, exclusion_list = load_config(config_file)
-
-    [globals().update(vars(importlib.import_module(module)))
-     for module in registry]
-
-    globals_list = globals()
-    return globals_list
