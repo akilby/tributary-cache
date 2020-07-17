04cbb49bb7fdb16db87665e9f19fd18f86075069
Angela E Kilby <a.kilby@northeastern.edu>
2020-07-16 22:03:04 -0400

    fixing bug created by pandas update to period columns - wont hash properly

--- cache/utils/objecthashers.py
+++ cache/utils/objecthashers.py
@@ -1,5 +1,5 @@
-import importlib
 import hashlib
+import importlib
 
 packages_list = []
 
@@ -34,7 +34,16 @@ def complex_hasher(obj):
     hasher_count = 0
     if 'pandas' in packages_list:
         if isinstance(obj, pd.DataFrame) or isinstance(obj, pd.Series):
-            out = hash_pandas_object(obj).sum()
+            # This should hopefully be temporary? something very weird
+            # about the period dtype, causing problems
+            # So converting to a timestamp dtype for the hasher
+            pds = obj.select_dtypes(pd.PeriodDtype)
+            if pds.empty:
+                out = hash_pandas_object(obj).sum()
+            else:
+                out = hash_pandas_object(
+                    obj.assign(**{col: obj[col].dt.to_timestamp()
+                                  for col in pds.columns})).sum()
             hasher_count += 1
     if 'scipy' in packages_list:
         if isinstance(obj, sp.csr.csr_matrix):
