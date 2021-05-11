974c52b12414289102a0cb59fd4b80ce296d6eff
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-11 05:10:51 -0400

    final clean up of major improvement to add decorator

--- cache/decorator.py
+++ cache/decorator.py
@@ -1,6 +1,8 @@
 import inspect
 from functools import wraps
 
+# THIS IS WRONG, BUT TEMPORARY - need to implement some sort of
+# module-based instruction
 path = '/scratch/akilby/Output/Cache/temp'
 exclusion_list = []
 
@@ -9,13 +11,12 @@ def cache_decorator(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
         from cache import cache
-        # THIS IS WRONG, BUT TEMPORARY
+
         module_to_import = inspect.getmodule(function).__name__
         c = cache.Cache(configure={'directory': path,
                                    'registry': [module_to_import],
                                    'exclusion_list': []},
                         noisily=True)
-        func = getattr(c, function.__code__.co_name)(*args, **kwargs)
-        return func
+        return getattr(c, function.__code__.co_name)(*args, **kwargs)
 
     return wrapper
