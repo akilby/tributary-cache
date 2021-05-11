767a175796987578793ea134366af12b0696f583
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-11 04:50:25 -0400

    adding finding function's module to decorator

--- cache/decorator.py
+++ cache/decorator.py
@@ -1,3 +1,4 @@
+import inspect
 from functools import wraps
 
 
@@ -5,6 +6,7 @@ def cache_decorator(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
         # THIS IS WRONG, BUT TEMPORARY
+        print(inspect.getmodule(function))
         from claims_data.utils.globalcache import c
         func = getattr(c, function.__code__.co_name)(*args, **kwargs)
         return func
