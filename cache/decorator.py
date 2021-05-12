2eb4385e330a9e36798f8c524b042742f15fa39a
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-12 01:39:53 -0400

    trying to fix the decorator registry attribute

--- cache/decorator.py
+++ cache/decorator.py
@@ -13,6 +13,8 @@ class Memoizer(object):
         Users should be able to declare, or use default temp directory
         Or, there can be a module-based config file like NPI, like already
         exists, and can retrieve config file once the module name is known
+        Should also have the option to turn off caching at the function level
+        Ask ryan best way
         """
 
         self.directory = directory
@@ -23,6 +25,8 @@ class Memoizer(object):
             os.mkdir(self.directory)
 
     def cache(self, function):
+
+        function.is_cacher_registered = True
 
         @functools.wraps(function)
         def wrapper(*args, **kwargs):
@@ -37,5 +41,5 @@ class Memoizer(object):
                             noisily=self.noisily)
             return getattr(c, function.__code__.co_name)(*args, **kwargs)
 
-        wrapper.is_cacher_registered = True
+        # wrapper.is_cacher_registered = True
         return wrapper
