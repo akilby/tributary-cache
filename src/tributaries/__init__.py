f40eb0a99cf8c7d6db220f242401e66929336edb
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-12 19:10:12 -0400

    test

--- src/tributaries/__init__.py
+++ src/tributaries/__init__.py
@@ -1,6 +1,6 @@
 from .decorator import Cacher
 
-package_name = 'tributaries'
+package_name = __name__
 
 dec_mod = '%s.decorator' % package_name
 util_mod = '%s.utils.universalmodules' % package_name
