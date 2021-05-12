d5dd1ce7aa6c4c18b4e85182ae64da49d6530398
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-12 01:16:41 -0400

    adding back old decorator

---
+++ cache/decorator2.py
@@ -0,0 +1,26 @@
+import functools
+
+
+def cache_decorator(function):
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+
+        module_to_import = function.__module__
+
+        from cache import cache
+
+        # THIS IS WRONG, BUT TEMPORARY - need to implement some sort of
+        # module-based instruction... something like joblib. Also rename
+        # the decorator and have it take arguments
+        path = '/scratch/akilby/Output/Cache/temp'
+        exclusion_list = []
+
+        c = cache.Cache(configure={'directory': path,
+                                   'registry': [module_to_import],
+                                   'exclusion_list': exclusion_list},
+                        noisily=True)
+        return getattr(c, function.__code__.co_name)(*args, **kwargs)
+
+    wrapper.is_cacher_registered = True
+    return wrapper
