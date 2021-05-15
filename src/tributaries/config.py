4b3bfa017e98809af7b25b571cd28c89c9d1a39d
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-15 01:20:06 -0400

    trying to add all configuration to package-specific file

--- src/tributaries/config.py
+++ src/tributaries/config.py
@@ -258,7 +258,11 @@ def configure_report(config_file=None):
 
 def configure_package(name):
     config_file = config_path(name=name)
-    noisily_def, rerun_def = config_defaults_package(config_file)
+    dir_def, noisily_def, rerun_def = config_defaults_package(config_file)
+    cache_directory = user_prompt("Directory for storing cache"
+                                  " files for this package (you probably don't"
+                                  "want this to be in your temp directory)",
+                                  default=dir_def)
     noisily = user_prompt('Cache noisily (y) or quietly (n)? ',
                           default=noisily_def,
                           inlist=['y', 'n'])
@@ -267,16 +271,20 @@ def configure_package(name):
                         inlist=['y', 'n'])
     noisily = True if noisily == 'y' else False
     rerun = True if rerun == 'y' else False
-    write_configs_package(config_file, noisily, rerun)
+    write_configs_package(config_file, cache_directory, noisily, rerun)
     print('Configuration file saved to %s' % config_file)
 
 
-def write_configs_package(config_file, noisily, rerun):
+def write_configs_package(config_file, cache_directory, noisily, rerun):
     """
     Writes configurations to file for package
     """
     with open(config_file, 'w') as f:
         writer = csv.writer(f, delimiter='\n')
+        writer.writerow(['Cache Directory:'])
+        writer.writerow([cache_directory])
+        writer.writerow([])
+        writer.writerow([])
         writer.writerow(['Noisily:'])
         writer.writerow([noisily])
         writer.writerow([])
@@ -293,13 +301,15 @@ def load_config_package(config_file):
         reader = csv.reader(f)
         config_dets = list(reader)
         config_dets = list(itertools.chain.from_iterable(config_dets))
-        idx1 = config_dets.index('Noisily:')
-        idx2 = config_dets.index('Rerun:')
-        noisily = single_item(config_dets[idx1+1:idx2])
-        rerun = single_item(config_dets[idx2+1:])
+        idx1 = config_dets.index('Cache Directory:')
+        idx2 = config_dets.index('Noisily:')
+        idx3 = config_dets.index('Rerun:')
+        directory = single_item(config_dets[idx1+1:idx2])
+        noisily = single_item(config_dets[idx2+1:idx3])
+        rerun = single_item(config_dets[idx3+1:])
     noisily = True if noisily == 'True' else False
     rerun = True if rerun == 'True' else False
-    return noisily, rerun
+    return directory, noisily, rerun
 
 
 def config_defaults_package(config_file):
@@ -312,5 +322,8 @@ def config_defaults_package(config_file):
         noisily = 'y' if noisily else False
         rerun = 'y' if rerun else False
     else:
-        (noisily, rerun) = ('None', 'None')
+        tempdir = os.path.join(tempfile.gettempdir(), '_cache')
+        if not os.path.isdir(tempdir):
+            os.mkdir(tempdir)
+        (noisily, rerun) = (tempdir, 'None', 'None')
     return noisily, rerun
